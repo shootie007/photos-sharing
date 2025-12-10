60a2235f726224a5139269916e1e9ba3d9e954ee
s-tada <s-tada@wwwave.jp>
2025-12-10 11:27:23 +0900

    写真アップロード機能を実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,5 +2,25 @@ class PhotosController < ApplicationController
   def index
     @photos = current_user.photos.order(created_at: :desc)
   end
+
+  def new
+    @photo = current_user.photos.build
+  end
+
+  def create
+    @photo = current_user.photos.build(photo_params)
+
+    if @photo.save
+      redirect_to photos_path, notice: "写真をアップロードしました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :image)
+  end
 end
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title, presence: true, length: { maximum: 30 }
-  validates :image, presence: true
+  validates :title, presence: { message: "タイトルが未入力です" }, length: { maximum: 30, message: "タイトルの文字数が30文字を超えています" }
+  validates :image, presence: { message: "画像ファイルが未入力です" }
 end
 
